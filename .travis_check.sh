0b0c0b978e17f44a17b0bb9bcfbbc92233d36f9c
oxmon-2500 <blachuta@gmx.net>
2020-02-14 19:10:20 +0100

    python 3 v22 inst pip3

--- .travis_check.sh
+++ .travis_check.sh
@@ -1,9 +1,13 @@
 #/bin/bash
 echo "------------------------------------------ .travis_check.sh ----------------------------------"
-      sudo apt-get update;
-      sudo apt-get install pip3; 
-      sudo find /usr /opt ! -type d -name "python3*" -ls;
-      sudo find /usr /opt ! -type d -name "pip3*" -ls;
-      /opt/python/3.6.3/bin/python3 --version;
-      export PATH=/opt/python/3.6.3/bin:$PATH;
+sudo apt-get update;
+sudo apt-get install pip3; 
+sudo find /usr /opt ! -type d -name "python3*" -ls;
+sudo find /usr /opt ! -type d -name "pip3*" -ls;
+PY3_PATH=$(sudo find /opt/python -name "python3")
+PY3_PATH=${PY3_PATH:0:-8}
+#/opt/python/3.6.3/bin/python3 --version;
+export PATH=${PY3_PATH}:$PATH;
+python3 --version
+pip3 --version
 echo "------------------------------------------ .travis_check.sh ----------------------------------"
