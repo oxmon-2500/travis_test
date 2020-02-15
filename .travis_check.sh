f6c14e1d18071fa375ed9a7d5a1e7401f24a6b52
oxmon-2500 <blachuta@gmx.net>
2020-02-15 13:21:26 +0100

    libQt3support osx v6 brew help

--- .travis_check.sh
+++ .travis_check.sh
@@ -15,10 +15,43 @@ python3 --version
 pip3 --version
 
 if [[ $TRAVIS_OS_NAME == osx ]]; then
+  brew cleanup
   brew tap cartr/qt4
   brew tap-pin cartr/qt4
   brew install qt@4
+  echo "---------------------------------brew --help -------------------------------------"
   brew --help
+  echo "---------------------------------brew instal --help -------------------------------------"
+  brew install --help
+  sudo find /usr /opt -iname "*Qt3Support*" -ls
+  #/usr/local/Cellar/qt@4/4.8.7_6/lib/Qt3Support.framework/Qt3Support -> Versions/4/Qt3Support
+  #/usr/local/Cellar/qt@4/4.8.7_6/lib/Qt3Support.framework/Versions/4/Qt3Support
+  clang++ --version
+  #clang++ -DHAVE_CONFIG_H -I. -I..    -I../qucs-lib -I/usr/local/lib/QtCore.framework/Headers -I/usr/local/lib/QtGui.framework/Headers -I/usr/local/lib/QtXml.framework/Headers -I/usr/local/lib/QtSvg.framework/Headers -I/usr/local/lib/QtScript.framework/Headers -I/usr/local/lib/QtTest.framework/Headers -I/usr/local/lib/Qt3Support.framework/Headers -DQT_SHARED -DQT3_SUPPORT -DQT3_SUPPORT_WARNINGS -DQT_DEPRECATED_WARNINGS -std=c++0x  -g -O2 -c -o main.o main.cpp
+  #clang++ -g -O2 -o qucs.real main.o qucs_.o -Wl,-bind_at_load  ./.libs/libqucsschematic.a -ldl -framework Qt3Support -framework QtTest -framework QtScript -framework QtSvg -framework QtXml -framework QtCore -framework QtGui
 fi
-sudo find /usr /opt -iname "*Qt3Support*" -ls
+
 echo "------------------------------------------ .travis_check.sh ----------------------------------"
+
+
+
+#Example usage:
+#  brew search [TEXT|/REGEX/]
+#  brew info [FORMULA...]
+#  brew install FORMULA...
+#  brew update
+#  brew upgrade [FORMULA...]
+#  brew uninstall FORMULA...
+#  brew list [FORMULA...]
+#Troubleshooting:
+#  brew config
+#  brew doctor
+#  brew install --verbose --debug FORMULA
+#Contributing:
+#  brew create [URL [--no-fetch]]
+#  brew edit [FORMULA...]
+#Further help:
+#  brew commands
+#  brew help [COMMAND]
+#  man brew
+#  https://docs.brew.sh
