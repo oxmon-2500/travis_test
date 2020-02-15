cc44be3b668c0a25a230272e823ff55354cdcec1
oxmon-2500 <blachuta@gmx.net>
2020-02-15 06:33:32 +0100

    plibQt3support

--- .travis_check.sh
+++ .travis_check.sh
@@ -1,13 +1,18 @@
 #/bin/bash
+TRAVIS_OS_NAME=$1
 echo "------------------------------------------ .travis_check.sh ----------------------------------"
-sudo apt-get update;
-sudo apt-get install pip3; 
+if [[ $TRAVIS_OS_NAME == linux ]]; then
+  sudo apt-get update
+  sudo apt-get install pip3
+fi
 sudo find /usr /opt ! -type d -name "python3*" -ls;
 sudo find /usr /opt ! -type d -name "pip3*" -ls;
 PY3_PATH=$(sudo find /opt/python -name "python3")
+# /opt/python/3.6.3/bin/python3 --> /opt/python/3.6.3/bin
 PY3_PATH=${PY3_PATH:0:-8}
-#/opt/python/3.6.3/bin/python3 --version;
 export PATH=${PY3_PATH}:$PATH;
 python3 --version
 pip3 --version
+
+sudo find /usr /opt ! -type d -name "*Qt3Support*" -ls;
 echo "------------------------------------------ .travis_check.sh ----------------------------------"
