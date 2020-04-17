60c028faf356f9864c82ed2b4417965f90def622
oxmon-2500 <blachuta@gmx.net>
2020-04-17 10:31:37 +0200

    test10

--- travis_check_gnucap.sh
+++ travis_check_gnucap.sh
@@ -1,13 +1,20 @@
 #/bin/bash
 TRAVIS_OS_NAME=$1
-echo "------------------------------------------ .travis_check_qt.sh ----------------------------------"
+echo "------------------------------------------ $0 ----------------------------------"
 sudo find /usr /opt ! -type d -name "qmake" -ls
 if [[ $TRAVIS_OS_NAME == linux ]]; then
   #sudo apt-get update
   sudo apt-get install gnucap
   #sudo apt-get install gnucap-dev
-  gnucap <<EOF
-  exit
-  EOF
+  gnucap < gnucap_cmd.txt # exit immediatly
+  wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz
+  tar -zxvf gsl-*.*.tar.gz
+  pushd gsl-*
+  echo "-------------------------username:$USERNAME"
+  ./configure --prefix=/home/$USERNAME/local/gsl
+  make
+  make check
+  make install
+  cmake --version
 fi
 
