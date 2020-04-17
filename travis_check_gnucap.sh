5be721405172d07a3ca6ab836635834835a1d3b2
oxmon-2500 <blachuta@gmx.net>
2020-04-17 10:12:46 +0200

    test09

--- travis_check_gnucap.sh
+++ travis_check_gnucap.sh
@@ -3,9 +3,11 @@ TRAVIS_OS_NAME=$1
 echo "------------------------------------------ .travis_check_qt.sh ----------------------------------"
 sudo find /usr /opt ! -type d -name "qmake" -ls
 if [[ $TRAVIS_OS_NAME == linux ]]; then
-  sudo apt-get update
+  #sudo apt-get update
   sudo apt-get install gnucap
-  sudo apt-get install gnucap-dev
-  gnucap --version
+  #sudo apt-get install gnucap-dev
+  gnucap <<EOF
+  exit
+  EOF
 fi
 
