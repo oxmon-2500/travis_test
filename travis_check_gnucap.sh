0b5c040bbbb105a2f89d422a9f13dbbfa280cc4f
oxmon-2500 <blachuta@gmx.net>
2020-04-17 12:28:09 +0200

    test15

--- travis_check_gnucap.sh
+++ travis_check_gnucap.sh
@@ -1,11 +1,52 @@
 #/bin/bash
 TRAVIS_OS_NAME=$1
 echo "------------------------------------------ $0 ----------------------------------"
-sudo find /usr /opt ! -type d -name "qmake" -ls
+DIST_LOCAL=~/local
+DIST_LOG=~/dist_log.log
+START_DIR=$(pwd);#/home/travis/build/oxmon-2500/travis_test
+
+function sysInfo(){
+  echo "--------------------------sysInfo------------------------"
+  #sudo find /usr /opt ! -type d -name "qmake" -ls
+}
+
+function instGnucap(){
+  echo "----------------------------------------gnucap--------------------------"
+  git clone git://git.sv.gnu.org/gnucap.git >> ${DIST_LOG}
+  pushd gnucap
+  git checkout develop
+  ./configure --prefix=${DIST_LOCAL}/gnucap
+  make  >> ${DIST_LOG}
+  make check
+  make install
+  popd # gnucap
+}
+
+function instGsl(){
+  echo "----------------------------------------gsl-GNU Scientific Library -------------------------"
+  wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz
+  tar -zxf gsl-*.*.tar.gz
+  pushd gsl-*
+  ./configure --prefix=${DIST_LOCAL}/gsl
+  make   >> ${DIST_LOG}
+  #make check
+  make install
+  popd # gsl
+}
+function instBlas(){
+  echo "----------------------------------------blas---Basic Linear Algebra Subprograms-----------------------"
+  # sudo apt-get install gfortran # Package gfortran is not available, but is referred to by another package.
+  # sudo apt-get install libblas-dev checkinstall
+  # CBLAS is C interface to the BLAS library
+  # https://www.netlib.org/blas/blas-3.8.0.tgz ??
+  git clone https://github.com/xianyi/OpenBLAS.git
+  pushd OpenBLAS
+  make >> ${DIST_LOG}
+  make PREFIX=${DIST_LOCAL}/blas install
+  popd # OpenBLAS
+}
+
 if [[ $TRAVIS_OS_NAME == linux ]]; then
-  DIST_LOCAL=~/local
-  DIST_LOG=~/dist_log.log
-  START_DIR=$(pwd);#/home/travis/build/oxmon-2500/travis_test
   #sudo apt-get update
   #sudo apt-get install gnucap
   #sudo apt-get install gnucap-dev
@@ -21,34 +62,20 @@ if [[ $TRAVIS_OS_NAME == linux ]]; then
   echo "-------------------------pwd:$(pwd)"
   mkdir sources
   pushd sources
-  echo "----------------------------------------gnucap--------------------------"
-  git clone git://git.sv.gnu.org/gnucap.git >> ${DIST_LOG}
-  pushd gnucap
-  git checkout develop
-  ./configure --prefix=${DIST_LOCAL}/gnucap
-  make  >> ${DIST_LOG}
-  make check
-  make install
-  popd # gnucap
+  instGnuCap()
+  echo "--------------------------------------------------------------------${LD_LIBRARY_PATH}"
+  export LD_LIBRARY_PATH=${DIST_LOCAL}/gnucap/lib:${LD_LIBRARY_PATH}
   ${DIST_LOCAL}/gnucap/bin/gnucap < ${START_DIR}/gnucap_cmd.txt # exit immediatly
   
-  echo "----------------------------------------gsl-GNU Scientific Library -------------------------"
-  wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz
-  tar -zxf gsl-*.*.tar.gz
-  pushd gsl-*
-  ./configure --prefix=${DIST_LOCAL}/gsl
-  make
-  #make check
-  make install
-  popd # gsl
-  
-  echo "----------------------------------------blas---Basic Linear Algebra Subprograms-----------------------"
-  sudo apt-get install gfortran
-  sudo apt-get install libblas-dev checkinstall
-  # CBLAS is C interface to the BLAS library
+  instGsl()
+
+  instBlas()    
   
   popd # sources
   
+  echo "--------------------------------------pwd:$(pwd)--------------------------------------"
+  
   cmake --version # 20200417;       cmake version 3.12.4
+  ls -l ${DIST_LOCAL}
 fi
 
